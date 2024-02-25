c5f759bf9055e2ef8949d3b08a7947634402c5f9
Justin Mach <bachmach24k@gmail.com>
2024-02-25 17:19:58 -0600

    udp changes

--- Udp.py
+++ Udp.py
@@ -1,42 +1,49 @@
 from splash_screen import Splash
 import socket
 
+LOCAL_IP = '127.0.0.1'
+BROADCAST_PORT = 7500
+RECEIVING_PORT = 7501
+BUFFER_SIZE = 1024
+
 class Udp:
 
-    def __init__(self, player_id) -> None:
-        self.__broadcast_port = 7500
-        self.__receive_port = 7501
-        self.player_id = player_id
+    def __init__(self) -> None:
+        self.__broadcast_port = BROADCAST_PORT
+        self.__broadcast_socket = broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.__receive_port = RECEIVING_PORT
+        self.__receive_socket = broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ 
 
         # Countdown timer finishes, transmit code 202
-        self.broadcast_data('202', self.__broadcast_port)
+        # self.broadcast_data('202', self.__broadcast_port)
 
-        # Example of receiving data and processing it
-        received_data = self.receive_data(self.__receive_port)
-        if received_data is not None:
-            transmission_parts = received_data.split(':')
+        # # Example of receiving data and processing it
+        # received_data = self.receive_data(self.__receive_port)
+        # if received_data is not None:
+        #     transmission_parts = received_data.split(':')
 
-            if len(transmission_parts) == 2:
-                transmitter_id, hit_id = map(int, transmission_parts)
+        #     if len(transmission_parts) == 2:
+        #         transmitter_id, hit_id = map(int, transmission_parts)
 
-                if transmitter_id == 53:
-                    # Red base scored
-                    # Process according to team color
-                    pass
-                elif transmitter_id == 43:
-                    # Green base scored
-                    # Process according to team color
-                    pass
-                else:
-                    # Player hit another player
-                    # Transmit their own equipment id
-                    self.broadcast_data(str(transmitter_id), self.__broadcast_port)
+        #         if transmitter_id == 53:
+        #             # Red base scored
+        #             # Process according to team color
+        #             pass
+        #         elif transmitter_id == 43:
+        #             # Green base scored
+        #             # Process according to team color
+        #             pass
+        #         else:
+        #             # Player hit another player
+        #             # Transmit their own equipment id
+        #             self.broadcast_data(str(transmitter_id), self.__broadcast_port)
 
-                    if hit_id == 1:
-                        self.broadcast_player_id()
-            # Game ends, transmit code 221 three times
-        for _ in range(3):
-            self.broadcast_data('221', self.__broadcast_port)
+        #             if hit_id == 1:
+        #                 self.broadcast_player_id()
+        #     # Game ends, transmit code 221 three times
+        # for _ in range(3):
+        #     self.broadcast_data('221', self.__broadcast_port)
 
     def broadcast_data(self, data, port):
         broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -46,6 +53,10 @@ class Udp:
         print("Searching for equipment ID of", self.broadcast_player_id)
         
         broadcast_socket.close()
+        
+    def broadcast_equipment_id(self, equipment_id):
+        self.__broadcast_socket.sendto(str(equipment_id).encode(), (LOCAL_IP, self.__broadcast_port))
+        print(f"Broadcasting equipment id: {equipment_id}......: ")
 
 
     def receive_data(self, port):
@@ -62,19 +73,3 @@ class Udp:
     
     def broadcast_player_id(self):
         self.broadcast_data(str(self.player_id), self.__broadcast_port)
-
-    # def main(player_id):
-    #     splash = Splash()
-    #     splash.root.mainloop()
-
-if __name__ == "__main__":
-    # Assuming player ID is 1 for testing purposes
-    player_id = 1
-
-    # Create an instance of the Udp class with the player ID
-    udp_instance = Udp(player_id)
-
-    # Here you can continue with the rest of your program logic
-
-
-    
