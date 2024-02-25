116f84a09e504d3dc30058eca1e3d2a35571e229
margaretmilne <margaretmilne31@gmail.com>
2024-02-25 15:35:02 -0600

    updated player entry screen

--- player_entry.py
+++ player_entry.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import ttk
+import tkinter.ttk as ttk
+from tkinter import simpledialog
 from database import Database
 
 class PlayerEntry:
@@ -14,14 +15,15 @@ class PlayerEntry:
         self.root.columnconfigure(1, weight=5)  # Set higher weight for column 1
 
         self.style = ttk.Style()
-        self.style.configure('Red.TLabelframe', background='#800000')  # Darker red
-        self.style.configure('Green.TLabelframe', background='green')
+        self.style.theme_use('clam')
+        self.style.configure('Red.TLabelframe', background='#800000', bordercolor= ' ')  # Darker red
+        self.style.configure('Green.TLabelframe', background='green', bordercolor = ' ')
 
         self.player_counts = {'Red': 1, 'Green': 1}
         self.player_entries = []
+        self.row_counters = {'Red': 2, 'Green': 2}  # Initial row counters for each team
 
         self.db = Database()
-        self.udp = Udp()
 
         self.create_team_frame("Red", 0)
         self.create_team_frame("Green", 1)        
@@ -29,38 +31,39 @@ class PlayerEntry:
         self.create_buttons()        
 
     def create_team_frame(self, team_color, column):
-        team_frame = ttk.LabelFrame(self.root, style=f'{team_color}.TLabelframe', borderwidth = 0)
+        team_frame = ttk.LabelFrame(self.root, style=f'{team_color}.TLabelframe')
         team_frame.grid(row=0, column=column, padx=5, pady=10, sticky="nsew")
         ttk.Label(team_frame, text=f"{team_color} Team", font=('Helvetica',15,'bold')).grid(row=0, column=3, sticky="w", padx=5, pady=5)
         self.create_player_entries(team_frame, team_color)
 
     def create_player_entries(self, parent_frame, team):
-        ttk.Label(parent_frame, text="User ID".format(team)).grid(row=1, column=2, sticky="w", padx=5, pady=(5,0), columnspan=2)
-        ttk.Label(parent_frame, text="Player Names").grid(row=1, column=5, padx=5, pady=(5,0), sticky="w", columnspan=2) 
+        ttk.Label(parent_frame, text="User ID".format(team)).grid(row=1, column=2, sticky="w", padx=5, pady=(5, 0), columnspan=2)
+        ttk.Label(parent_frame, text="Player Names").grid(row=1, column=5, padx=5, pady=(5, 0), sticky="w", columnspan=2)
+
+        def add_player(team):
+            def inner_add_player():
+                player_id = simpledialog.askinteger("ID", "Enter Player ID")
+                print(player_id)
+                if player_id:
+                    user_id_var = tk.StringVar()
+                    user_id_var.set(player_id)
+                    print(user_id_var)
+                    user_id_entry = ttk.Label(parent_frame, text=player_id, state='readonly')
+                    user_id_entry.grid(row=self.row_counters[team], column=2, padx=5, pady=5, columnspan=2)
+
+                    player_name = simpledialog.askstring("Name", "Enter Player Name")
+                    if player_name:
+                        player_name_var = tk.StringVar()
+                        player_name_var.set(player_name)
+                        player_name_entry = ttk.Label(parent_frame, text=player_name, state='readonly')
+                        player_name_entry.grid(row=self.row_counters[team], column=5, padx=5, pady=5, columnspan=2)
+
+                        print("Adding player to team: ", team)
+                        self.row_counters[team] += 1  # Increment row counter for the team
+
+            return inner_add_player
         
-        def add_player():
-            print ("Adding player to team: ", team)
-
-            # Create new user id fields
-            entry = ttk.Entry(parent_frame, width=20)
-            entry.grid(row=len(self.player_entries) + 2, column=2, padx=5, pady=5, sticky="w")
-            
-            # create new player entry fields
-            entry_id = ttk.Entry(parent_frame, width=20)
-            entry_id.grid(row=len(self.player_entries) + 2, column=5, padx=5, pady=5, sticky="w")
-
-            player_number = self.player_counts[team]  # Get the current player count for the team
-            ttk.Label(parent_frame, text="{}: ".format(player_number)).grid(row=len(self.player_entries) + 2, column=0, padx=5, pady=5, sticky="e")  
-            print(self.player_counts[team])
-            self.player_counts[team] += 1  # Increment the player count for the team
-
-            print(self.player_counts[team])
-
-            self.player_entries.append((entry, entry_id))
-
-        add_player()
-
-        ttk.Button(parent_frame, text="Add Player", command=add_player).grid(row=17, column=2, columnspan=5, pady=5)
+        ttk.Button(parent_frame, text="Add Player", command=add_player(team)).grid(row=17, column=2, columnspan=5, pady=5)
 
 
     def create_buttons(self):
@@ -69,14 +72,14 @@ class PlayerEntry:
             'F1': '      F1\nEdit Game',
             'F2': '             F2\nGame Parameters',
             'F3': '       F3\nStart Game',
-            'F5': '             F5\nPre-Entered Games',
+            'F5': '              F5\nPre-Entered Games',
             'F7': ' F7\n',
             'F8': '        F8\nView Game',
             'F10': '     F10\nFlick Sync',
             'F12': '      F12\nClear Game'
         }
 
-        button_frame = tk.Frame(self.root, background = "#7F886E")
+        button_frame = tk.Frame(self.root, bg = "#7F886E")
         button_frame.grid(row = 10, column = 0, columnspan = 2, sticky = "s", padx = 10, pady = 10)
 
         for idx, (key, value) in enumerate(buttons.items()):
