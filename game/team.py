fc195ddac45a3023443097b23b91c493679f1d80
Justin Mach <bachmach24k@gmail.com>
2024-04-04 14:01:32 -0500

    change: team attributes to be private

--- game/team.py
+++ game/team.py
@@ -1,24 +1,25 @@
-from player import Player
+from game.player import Player
 from typing import List
 class Team:
     def __init__(self, team_name: str) -> None:
-        self.team_name: str = team_name
-        self.players: List[Player] = []
+        self.__team_name: str = team_name
+        self.__players: List[Player] = []
         self.points: int = 0
         
     def add_player_to_team(self, player: Player):
-        self.players.append(player)
+        self.__players.append(player)
     
     def remove_player_from_team(self, player):
-        self.players.remove(player)
+        self.__players.remove(player)
         
     def clear_team(self):
-        self.players = []
+        self.__players = []
         
     def get_points(self):
         return self.points
     
+    def get_team_size(self):
+        return len(self.__players)
+    
     def update_points(self):
-        self.points = sum([player.get_points() for player in self.players])
-        
-        
+        self.points = sum([player.get_points() for player in self.__players])
