f890add61a3e33704187886a1b8b0bf198d17d06
Justin Mach <bachmach24k@gmail.com>
2024-04-21 22:02:23 -0500

    add: end code broadcast on timer end

--- components/timer_box.py
+++ components/timer_box.py
@@ -5,7 +5,7 @@ import os
 import pygame
 from game.graceful_thread import GracefulThread
 import time
-import asyncio
+from game.udp import udp
 import random
 
 TIMER_WINDOW_WIDTH = 200
@@ -59,6 +59,8 @@ class TimerWindow(ttk.Frame):
             time_string = f"{minutes:02d}:{seconds:02d}"
             self.timer_label.config(text=time_string)
             self.after(1000, lambda: self.start_game_timer(total_seconds - 1))
+        else:
+            udp.broadcast_end()
 
     # def play_music(self):
     #     music_path = os.path.join("Tracks/", "Track08.wav")
