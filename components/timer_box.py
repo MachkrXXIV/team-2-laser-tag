9512661a7c7dc06ccf332d96060f6e0bbff8d860
Justin Mach <bachmach24k@gmail.com>
2024-04-21 21:28:52 -0500

    add:  pygame for sound

--- components/timer_box.py
+++ components/timer_box.py
@@ -4,11 +4,12 @@ from PIL import Image, ImageTk
 import os
 import winsound
 from game.graceful_thread import GracefulThread
-import time 
+import time
 import asyncio
 
 TIMER_WINDOW_WIDTH = 200
 TIMER_WINDOW_HEIGHT = 100
+
 
 class TimerWindow(ttk.Frame):
     def __init__(self, parent):
@@ -22,31 +23,32 @@ class TimerWindow(ttk.Frame):
         self.style = ttk.Style()
         self.style.theme_use("clam")
         self.style.configure("TimerWindow.TFrame", background="white")
-        self.style.configure("TimerLabel.TLabel", foreground="black", background="white")
+        self.style.configure(
+            "TimerLabel.TLabel", foreground="black", background="white"
+        )
 
         self.configure(style="TimerWindow.TFrame")
-        self.digit_images = [ImageTk.PhotoImage(Image.open(os.path.join("images", f"{i}.tif"))) for i in range(6)]
+        self.digit_images = [
+            ImageTk.PhotoImage(Image.open(os.path.join("images", f"{i}.tif")))
+            for i in range(6)
+        ]
         self.music_thread.start()
         time.sleep(10)
         self.start_countdown_timer()
-        
-        
 
     def start_countdown_timer(self, count=5):
         if count >= 0:
             self.timer_label.config(image=self.digit_images[count])
             self.timer_label.after(2000, lambda: self.start_countdown_timer(count - 1))
         else:
-               
+
             self.timer_label.config(image="")
-            
+
             self.timer_label.destroy()
             self.timer_label = ttk.Label(self, text="06:30", font=("Helvetica", 24))
             self.timer_label.pack(expand=True)
-            
+
             self.start_game_timer(390)
-            
-        
 
     def start_game_timer(self, total_seconds):
         if total_seconds >= 0:
@@ -56,8 +58,6 @@ class TimerWindow(ttk.Frame):
             self.timer_label.config(text=time_string)
             self.after(1000, lambda: self.start_game_timer(total_seconds - 1))
 
-
     def play_music(self):
         music_path = os.path.join("Tracks/", "Track08.wav")
         winsound.PlaySound(music_path, winsound.SND_FILENAME)
-
